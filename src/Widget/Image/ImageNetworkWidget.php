bc419fa7f8b757e3b59d987c1a4800bf94fb2a89
Máximo Sojo <maxsojo13@gmail.com>
2025-09-28 11:46:50 -0400

    refactor(Image):Se agrega widget de imagenes

---
+++ src/Widget/Image/ImageNetworkWidget.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * This file is part of the Máximo Sojo - package.
+ * 
+ * (c) https://maximosojo.github.io/
+ * 
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Maximosojo\UIBuilderPHP\Widget\Image;
+
+use Maximosojo\UIBuilderPHP\Widget\AbstractWidget;
+
+/**
+ * Widget para ImageNetworkWidget
+ *
+ * @author Máximo Sojo <maxsojo13@gmail.com>
+ */
+class ImageNetworkWidget extends AbstractWidget
+{
+	public function __construct($src)
+	{
+        parent::__construct("image_network");
+        $this->setProperty('src', $src);
+    }
+
+    public function setSrc($src)
+    {
+        $this->setProperty('src', $src);
+
+        return $this;
+    }
+}
